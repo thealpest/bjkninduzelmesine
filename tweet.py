03c16b5147bfa4ed996ce07af9a0e293ae86e25e
thealpest <45305458+thealpest@users.noreply.github.com>
2025-08-04 23:52:22 +0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,18 +1,43 @@
 import os
+from datetime import datetime, timedelta, timezone
 import tweepy
 
-# V2 Auth (Bearer Token + OAuth2)
+# Twitter API kimlik bilgileri ve Bearer Token
+bearer_token = os.environ["BEARER_TOKEN"]
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+# Tweepy Client (v2 API)
 client = tweepy.Client(
-    bearer_token=os.environ["BEARER_TOKEN"],
-    consumer_key=os.environ["CONSUMER_KEY"],
-    consumer_secret=os.environ["CONSUMER_SECRET"],
-    access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    bearer_token=bearer_token,
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
 )
 
+# Geri sayım tarihi (2 Temmuz 2028, 16:30 GMT+3)
+deadline = datetime(2028, 7, 2, 16, 30, tzinfo=timezone(timedelta(hours=3)))
+
+# Şu anki zaman (GMT+3)
+now = datetime.now(timezone(timedelta(hours=3)))
+
+# Kalan gün sayısı
+days_left = (deadline.date() - now.date()).days
+
+# Tweet metni
+if days_left > 0:
+    tweet = f"{days_left} gün kaldı."
+elif days_left == 0:
+    tweet = "Bugün!"
+else:
+    tweet = "Etkinlik sona erdi."
+
+# Tweet atma işlemi
 try:
-    tweet = "Bu bir test tweetidir. #v2test"
     response = client.create_tweet(text=tweet)
     print("Tweet atıldı. Tweet ID:", response.data["id"])
 except Exception as e:
-    print("Hata oluştu:", e)
+    print("Tweet gönderilirken hata oluştu:", e)
