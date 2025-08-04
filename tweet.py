e79994540af8569ba29cd63fb442d9d7b57d202f
thealpest <45305458+thealpest@users.noreply.github.com>
2025-08-04 15:33:44 +0300

    Update tweet.py
    deneme

--- tweet.py
+++ tweet.py
@@ -1,29 +1,29 @@
-import tweepy
-import datetime
 import os
+import tweepy
 
-# Twitter API bilgilerini ortam değişkenlerinden al
-api_key = os.environ["TWITTER_API_KEY"]
-api_secret = os.environ["TWITTER_API_SECRET"]
-access_token = os.environ["TWITTER_ACCESS_TOKEN"]
-access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+# Environment variable'lardan API anahtarlarını al
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
-# Twitter API bağlantısı kur
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+# OAuth1 ile kimlik doğrulama
+auth = tweepy.OAuth1UserHandler(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
 api = tweepy.API(auth)
 
-# Hedef tarih: 2 Temmuz 2028 saat 16:30
-target = datetime.datetime(2028, 7, 2, 16, 30)
-now = datetime.datetime.now()
-delta_days = (target - now).days
+try:
+    me = api.verify_credentials()
+    print("Giriş başarılı:", me.screen_name)
 
-# Tweet içeriği
-if delta_days > 0:
-    tweet = f"{delta_days} gün kaldı."
-elif delta_days == 0:
-    tweet = "Bugün!"
-else:
-    tweet = "Etkinlik geçti."
+    tweet = "Bu bir test tweetidir. #test"
+    api.update_status(tweet)
 
-# Tweet gönder
-api.update_status(tweet)
+    print("Tweet atıldı.")
+except Exception as e:
+    print("Hata oluştu:", e)
