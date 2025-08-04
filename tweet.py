f852f613e7691b5f7acffa85b628cb1e765e8bd7
thealpest <45305458+thealpest@users.noreply.github.com>
2025-08-04 23:28:29 +0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,19 +1,24 @@
 import os
 import tweepy
 
-consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
-consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
-access_token = os.environ["TWITTER_ACCESS_TOKEN"]
-access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+# Ortam değişkenlerinden al
+bearer_token = os.environ["BEARER_TOKEN"]
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+# V2 Client ile tweet at
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
 
 try:
-    me = api.verify_credentials()
-    print("Giriş başarılı:", me.screen_name)
-    tweet = "Bu bir test tweetidir. #test"
-    api.update_status(tweet)
-    print("Tweet atıldı.")
+    response = client.create_tweet(text="Bu bir V2 test tweetidir. #test")
+    print("Tweet atıldı. Tweet ID:", response.data["id"])
 except Exception as e:
     print("Hata oluştu:", e)
