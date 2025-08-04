3c8eaf6118af82d3748d324109aaee7e72759de2
thealpest <45305458+thealpest@users.noreply.github.com>
2025-08-04 23:36:43 +0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,24 +1,18 @@
 import os
 import tweepy
 
-# Ortam değişkenlerinden al
-bearer_token = os.environ["BEARER_TOKEN"]
-consumer_key = os.environ["CONSUMER_KEY"]
-consumer_secret = os.environ["CONSUMER_SECRET"]
-access_token = os.environ["ACCESS_TOKEN"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
-
-# V2 Client ile tweet at
+# V2 Auth (Bearer Token + OAuth2)
 client = tweepy.Client(
-    bearer_token=bearer_token,
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret
+    bearer_token=os.environ["BEARER_TOKEN"],
+    consumer_key=os.environ["CONSUMER_KEY"],
+    consumer_secret=os.environ["CONSUMER_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
 )
 
 try:
-    response = client.create_tweet(text="Bu bir V2 test tweetidir. #test")
+    tweet = "Bu bir test tweetidir. #v2test"
+    response = client.create_tweet(text=tweet)
     print("Tweet atıldı. Tweet ID:", response.data["id"])
 except Exception as e:
     print("Hata oluştu:", e)
